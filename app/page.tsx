72e849fcbcf70af4503cd337320bdf55780d51d8
ammar_ib <ammaryasir8088@gmail.com>
2026-09-12 20:32:42 +0300

    fix error

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,12 @@ import { redirect } from 'next/navigation';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/lib/auth';
 
+export const dynamic = 'force-dynamic';
+
 export default async function Home() {
   const session = await getServerSession(authOptions);
-
   if (session) {
     redirect('/dashboard');
-  } else {
-    redirect('/signin');
   }
+  redirect('/signin');
 }
